3991efe55384d80fefef343482090448c3584d80
Janetyu <931242644@qq.com>
2018-10-15 15:29:25 +0800

    添加对日志输出的相关注释

--- Gin/gin-restful-learn/apiserver/handler/user/create.go
+++ Gin/gin-restful-learn/apiserver/handler/user/create.go
@@ -25,15 +25,19 @@ func Create(c *gin.Context) {
 
 	log.Debugf("username is:[%s], password is [%s]", r.Username, r.Password)
 	if r.Username == "" {
+		// 返回给用户的是 {"code":20102,"message":"The user was not found. This is add message."}
 		err = errno.New(errno.ErrUserNotFpund, fmt.Errorf("username can not found in db:xx.xx.xx.xx")).Add("This is add message.")
+		// 输出到日志文件
 		log.Errorf(err, "Get an error")
 	}
 
 	if errno.IsErrUserNotFound(err) {
+		// 输出到运行日志
 		log.Debug("err type is ErrUserNotFound")
 	}
 
 	if r.Password == "" {
+		// 输出到后台日志
 		err = fmt.Errorf("password is empty")
 	}
 
